43cff54e2e157b072695b352b953655c41f867dd
Rake <Rake@Noks.org>
2024-03-21 17:28:07 +0300

    fix:svgIcons

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				{/* <link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
+					integrity="sha512-NmLkDIU1C/C88wi324HBc+S2kLhi08PN5GDeUVVVC/BVt/9Izdsc9SVeVfA1UZbY3sHUlDSyRXhCzHfr6hmPPw=="
+					crossOrigin="anonymous"
+					referrerPolicy="no-referrer"
+				/> */}
+			</head>
 			<Provider store={store}>
 				<body className={inter.className}>{children}</body>
 			</Provider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,265 +1,107 @@
-"use client";
+import React from "react";
 import styles from "./page.module.css";
 import styled from "styled-components";
-import React, {
-	Suspense,
-	createContext,
-	lazy,
-	useContext,
-	useEffect,
-	useId,
-	useReducer,
-	useRef,
-	useState,
-} from "react";
-import { Button } from "@/components";
-import Link from "next/link";
-import { useDispatch } from "react-redux";
-import { typeAppDispath } from "@/storeRedux/store";
-import { sliceActions } from "@/storeRedux/slice";
-import { Controller, useForm } from "react-hook-form";
 import cn from "classnames";
-const LazyDownload = lazy(() => delayForDemo(import("./test")));
 
-function delayForDemo(promise) {
-	return new Promise((resolve) => {
-		setTimeout(resolve, 2000);
-	}).then(() => promise);
-}
-
-function formRed(state, action) {
-	// console.log(action.payload);
-	switch (action.type) {
-		case "CLEAR":
-			return (state = state + 1);
-		case "Fill":
-			return {
-				values: action.payload,
-			};
-	}
-}
-
-function useLocation() {
-	const [data, setData] = useState();
-
-	return [data, savetData];
-}
-
-export const UserContext = createContext({ userId: 1 });
+// ---svgrIcons---
+import MoonSvg from "./assets/icons/moon.svg";
+import SunSvg from "./assets/icons/sun.svg";
+import Link from "next/link";
+import { Navbar } from "react-bootstrap";
+import Image from "next/image";
+// function useLocation() {
+// 	const [data, setData] = useState();
+//  CREATE_CUSTOM_HOOK
+// 	return [data, savetData];
+// }
 export default function Home(): JSX.Element {
-	const id = useId();
-	const [count, setCount] = useState<string>("change");
-	const [formState, dispatch] = useReducer(formRed, 0);
-	const { values } = formState;
-	console.log(values);
-	const refTitle = useRef();
-	const refPs = useRef();
-
-	const inc = () => {
-		setCount((state) => (state = "saved"));
-		return true;
-	};
-	const [item, setItem] = useState([{}]);
-	const numb = 22;
-	useEffect(() => {
-		dispatch({ type: "Fill", payload: numb });
-		console.log(formState);
-	}, [item]);
-	const focus = (reff) => {
-		reff.current.focus();
-	};
-
-	const clearInput = (event, id) => {
-		for (let i = 0; i < id; i++) {
-			event.target[i].value = "";
-		}
-	};
-
-	const addItem = (event) => {
-		event.preventDefault();
-		const formData = new FormData(event.target);
-
-		const formProps = Object.fromEntries(formData);
-		const { name, password } = formProps;
-		console.log(formProps);
-		if (name == "") {
-			focus(refTitle);
-			return;
-		} else if (password == "") {
-			focus(refPs);
-			return;
-		}
-
-		setItem((state) => [
-			...state,
-			{
-				name: formProps.name,
-				text: formProps.password,
-			},
-		]);
-		clearInput(event, 2);
-	};
-	console.log(item);
-
-	function getNUm(a, b) {
-		if (a % 2 === 0 && b % 2 === 0) {
-			return "even";
-		}
-	}
-	console.log(getNUm(2, 2));
-	const [num, setNumber] = useState([]);
-	const [loading, setLoading] = useState(false);
-	useEffect(() => {
-		setLoading((state) => (state = true));
-		setTimeout(() => {
-			const sumer = async () => {
-				const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-				const response = await res.json();
-				const sum = response.map((el, i) => el.id);
-				// return response;
-				console.log(sum);
-				setNumber(sum);
-				// return response;
-			};
-			sumer();
-			setLoading((state) => (state = false));
-		}, 3000);
-	}, []);
-	console.log(num);
-	// }, []);
-	// console.log(getlet());
-	const some = useContext(UserContext);
-
-	const Title = styled.h1<{ $primary?: boolean }>`
-		font-size: 34px;
-		color: red;
-		padding: 25px 0;
-		background-color: ${({ $primary }) => ($primary ? "orange" : "gray")};
-		text-align: center;
-	`;
-
-	const dispatcch = useDispatch<typeAppDispath>();
-	dispatcch(sliceActions.show("asd"));
-	dispatcch(sliceActions.getler("12345qwerty"));
-	const {
-		register,
-		control,
-		handleSubmit,
-		reset,
-		formState: { errors },
-	} = useForm();
-
-	const onSubmitt = (data) => {
-		console.log(data);
-		reset();
-	};
 	return (
 		<>
-			{/* <Link /> */}
-			<Title $primary>Hello wolrd</Title>
+			<Navbar className={styles["nav"]}>
+				<Link href="/" className={styles["logo"]}>
+					<strong>portfolio</strong>
+				</Link>
 
-			<form className={styles.form} onSubmit={handleSubmit(onSubmitt)}>
-				<div className={styles.wrapper}>
-					<label className={styles.label} htmlFor={`${id}-name`}>
-						name
-					</label>
+				<ul className={styles["nav-list"]}>
+					<li className={styles["nav-list__item"]}>
+						<Link href="/" className={styles["nav-list__link nav-list__link--active"]}>
+							Home
+						</Link>
+					</li>
+					<li className={styles["nav-list__item"]}>
+						<Link href="/projects" className={styles["nav-list__link"]}>
+							Projects
+						</Link>
+					</li>
+					<li className={styles["nav-list__item"]}>
+						<Link href="/contacts" className={styles["nav-list__link"]}>
+							Contacts
+						</Link>
+					</li>
+				</ul>
+				<button className={styles["dark-mode-btn"]}>
+					<MoonSvg className={styles["dark-mode-btn__icon"]} />
+					<SunSvg className={styles["dark-mode-btn__icon"]} />
+				</button>
+			</Navbar>
 
-					<input
-						className={styles.input}
-						{...register("email", { required: { value: true, message: "typing please" } })}
-						id={`${id}-name`}
-						placeholder="email"
-
-						// ref={refPs}
-					/>
-					{errors.email && (
-						<span
-							className={cn({
-								[styles.errors]: errors.email,
-							})}
-						>
-							{errors.email.message}
-						</span>
-					)}
-				</div>
-				<Controller
-					control={control}
-					name="someThing"
-					render={({ field: { value, name }, fieldState, formState }) => {
-						return (
-							<input
-								type="text"
-								placeholder={name}
-								{...register("someThing", { required: true })}
-							/>
-						);
-					}}
-				/>
-
-				<div className={styles.wrapper}>
-					<label className={styles.label} htmlFor={`${id}-pswd`}>
-						password
-					</label>
-					<input
-						className={styles.input}
-						{...register("password", {
-							required: { value: true, message: "password" },
-						})}
-						id={`${id}-pswd`}
-						type="password"
-						// ref={refPs}
-						placeholder="password"
-						name="password"
-					/>
-					{errors.password && (
-						<span
-							className={cn({
-								[styles.errors]: errors.password,
-							})}
-						>
-							{errors.password.message}
-						</span>
-					)}
-				</div>
-				<div className={styles.fb}>
-					<UserContext.Provider value={{ userId: 1 }}>
-						<Button context={some.userId} text="Submit" onClick={reset} />
-						<Button context={some.userId + 1} text="Reset" onClick={reset} />
-					</UserContext.Provider>
-				</div>
-				<input type="button" value="reset" onClick={() => reset()} />
-			</form>
-
-			{item.map((el, i) =>
-				i == 0 ? (
-					<></>
-				) : (
-					<div key={i}>
-						{el.name} - {el.text}
+			<header className={styles["header"]}>
+				<div className={styles["header__wrapper"]}>
+					<h1 className={styles["header__title"]}>
+						<strong>
+							Hi, my name is <em>Sergey</em>
+						</strong>
+						<br />a Web developer
+					</h1>
+					<div className={styles["header__text"]}>
+						<p>with passion for learning and creating.</p>
 					</div>
-				),
-			)}
-			<Suspense fallback={"Загрузочка..."}>
-				<LazyDownload />
-			</Suspense>
-			<div>
-				{loading && <>Загрузка...</>}
-				{num.map((el, i) => (
-					<div key={i}>{el}</div>
-				))}
-			</div>
+					<a href="#!" className={styles["btn"]}>
+						Download CV
+					</a>
+				</div>
+			</header>
+
+			<main className={styles["section"]}>
+				<div className={styles["container"]}>
+					<ul className={styles["content-list"]}>
+						<li className={styles["content-list__item"]}>
+							<h2 className={styles["title-2"]}>Frontend</h2>
+							<p>
+								JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap,
+								MaterialUI, Yarn, TailwindCSS, StyledComponents
+							</p>
+						</li>
+						<li className={styles["content-list__item"]}>
+							<h2 className={styles["title-2"]}>Backend</h2>
+							<p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+						</li>
+					</ul>
+				</div>
+			</main>
+
+			<footer className={styles["footer"]}>
+				<ul className={styles["social"]}>
+					<li className={styles["social__item"]}>
+						<a href="#!">
+							<Image src="/images/icons/vk.svg" width={40} height={40} alt="vk" />
+						</a>
+					</li>
+					<li className={styles["social__item"]}>
+						<a href="#!">
+							<Image src="/images/icons/instagram.svg" width={40} height={40} alt="instagram" />
+						</a>
+					</li>
+					<li className={styles["social__item"]}>
+						<a href="#!">
+							<Image src="/images/icons/gitHub.svg" width={40} height={40} alt="gitHub" />
+						</a>
+					</li>
+				</ul>
+				<div className={styles["copyright"]}>
+					<p>© 2024 web-developer.com</p>
+				</div>
+			</footer>
 		</>
 	);
 }
-
-// export default class Home extends React.Component {
-// 	render() {
-// 		return (
-// 			<>
-// 				<Button text="Hello kira" />
-// 				<br />
-// 				<button>bos</button>
-// 			</>
-// 		);
-// 	}
-// }
