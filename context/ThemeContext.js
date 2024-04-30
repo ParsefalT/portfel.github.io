6407515b5bb2342656bda74ceea4a65c8a690c7f
Rake <Rake@Noks.org>
2024-04-30 23:03:55 +0300

    use

---
+++ context/ThemeContext.js
@@ -0,0 +1,30 @@
+// /components/ThemeContext.js
+import { createContext, ReactNode, useContext, useLayoutEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+	const [theme, setTheme] = useState("dark");
+
+	useLayoutEffect(() => {
+		const localTheme = window.localStorage.getItem("theme");
+		if (localTheme) {
+			setTheme(localTheme);
+		}
+		if (localTheme === "light") {
+			document.body.classList.add("light");
+		} else {
+			document.body.classList.remove("light");
+		}
+	}, [theme]);
+
+	const toggleTheme = () => {
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+		window.localStorage.setItem("theme", newTheme);
+	};
+
+	return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
